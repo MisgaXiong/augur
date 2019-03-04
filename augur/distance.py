0ec17914e19116ac025940ce75d8a4bf52dfdbf1
John Huddleston <huddlej@gmail.com>
2019-03-04 12:35:32 -0800

    Calculate distances using distance maps
    Adds support for reading distance map JSONs and calculating distances between
    sequences using a given map and the sequences in a given tree. This support
    replaces the original mask-based definitions for calculating distances and
    provides a more generic way to calculate distances between sequences.
    
    This commit only adds support for comparisons between all nodes and the tree's
    root. Future commits will add support for comparison to the last ancestor in a
    previous season or pairwise comparisons to all samples in previous seasons.

--- augur/distance.py
+++ augur/distance.py
@@ -4,6 +4,7 @@ import argparse
 import Bio
 import Bio.Phylo
 from collections import defaultdict
+import copy
 import json
 import numpy as np
 import os
@@ -20,6 +21,144 @@ def read_masks(mask_file):
             ha_masks[key] = np.frombuffer(value.encode(), 'S1').astype(int).astype(bool)
 
     return ha_masks
+
+
+def read_distance_map(map_file):
+    """Read a distance map JSON into a dictionary and assert that the JSON follows
+    the correct format. Coordinates should be one-based in the JSON and are
+    converted to zero-based coordinates on load.
+
+    Parameters
+    ----------
+    map_file : str
+        name of a JSON file containing a valid distance map
+
+    Returns
+    -------
+    dict :
+        Python representation of the distance map JSON
+
+    >>> sorted(read_distance_map("tests/data/distance_map_weight_per_site.json").items())
+    [('default', 0), ('map', {'HA1': {144: 1}})]
+
+    """
+    # Load the JSON.
+    with open(map_file, "r") as fh:
+        json_distance_map = json.load(fh)
+
+    # Confirm that all required fields are present.
+    assert "default" in json_distance_map
+    assert "map" in json_distance_map
+
+    # Convert coordinate numbers from strings to integers.
+    distance_map = copy.deepcopy(json_distance_map)
+    for gene, site_weights in json_distance_map["map"].items():
+        for site, weights in site_weights.items():
+            # Convert each one-based site string to a zero-based integer.
+            distance_map["map"][gene][int(site) - 1] = weights
+            del distance_map["map"][gene][site]
+
+    # Return the distance map.
+    return distance_map
+
+
+def get_distance_between_nodes(node_a_sequences, node_b_sequences, distance_map):
+    """Calculate distance between the two given nodes using the given distance map.
+
+    In cases where the distance map between sequences is asymmetric, the first
+    node is interpreted as the "ancestral" sequence and the second node is
+    interpreted as the "derived" sequence.
+
+    Parameters
+    ----------
+    node_a_sequences, node_b_sequences : dict
+
+    distance_map : dict
+
+    Returns
+    -------
+    int or float :
+        distance between node sequences based on the given map where the
+        returned type matches the type of the default value
+
+    >>> node_a_sequences = {"gene": "ACTG"}
+    >>> node_b_sequences = {"gene": "ACGG"}
+    >>> distance_map = {"default": 0, "map": {}}
+    >>> get_distance_between_nodes(node_a_sequences, node_b_sequences, distance_map)
+    0
+    >>> distance_map = {"default": 1, "map": {}}
+    >>> get_distance_between_nodes(node_a_sequences, node_b_sequences, distance_map)
+    1
+    >>> distance_map = {"default": 0.0, "map": {"gene": {3: 1.0}}}
+    >>> get_distance_between_nodes(node_a_sequences, node_b_sequences, distance_map)
+    0.0
+    >>> distance_map = {"default": 0.0, "map": {"gene": {2: 0.5, 3: 1.0}}}
+    >>> get_distance_between_nodes(node_a_sequences, node_b_sequences, distance_map)
+    0.5
+
+    """
+    distance_type = type(distance_map["default"])
+    distance = distance_type(0)
+
+    for gene in node_a_sequences:
+        gene_length = len(node_a_sequences[gene])
+
+        for site in range(gene_length):
+            if node_a_sequences[gene][site] != node_b_sequences[gene][site]:
+                if gene in distance_map["map"] and site in distance_map["map"][gene]:
+                    # Assume distances are site- and sequence-specific
+                    # first. Failing that, distances must be site-specific.
+                    seq_ancestral = node_a_sequences[gene][site]
+                    seq_derived = node_b_sequences[gene][site]
+
+                    try:
+                        distance += distance_map["map"][gene][site][(seq_ancestral, seq_derived)]
+                    except TypeError:
+                        distance += distance_map["map"][gene][site]
+                else:
+                    distance += distance_map["default"]
+
+    return distance_type(distance)
+
+
+def get_distances_to_root(tree, sequences_by_node_and_gene, distance_map):
+    """Calculate distances between all samples in the given sequences and the node
+    of the given tree using the given distance map.
+
+    Parameters
+    ----------
+    tree : Bio.Phylo
+        a rooted tree whose node names match the given dictionary of sequences
+        by node and gene
+
+    sequences_by_node_and_gene : dict
+        nucleotide or amino acid sequences by node name and gene
+
+    distance_map : dict
+        site-specific and, optionally, sequence-specific distances between two
+        sequences
+
+    Returns
+    -------
+    dict :
+        distances calculated between the root sequence and each sample in the
+        tree and indexed by node name
+
+    """
+    distances_by_node = {}
+
+    # Find the root node's sequences.
+    root_node_sequences = sequences_by_node_and_gene[tree.root.name]
+
+    # Calculate distance between root and all other nodes.
+    for node_name, node_sequences in sequences_by_node_and_gene.items():
+        distances_by_node[node_name] = get_distance_between_nodes(
+            root_node_sequences,
+            node_sequences,
+            distance_map
+        )
+
+    return distances_by_node
 
 
 def mask_sites(aa, mask):
@@ -51,18 +190,16 @@ def register_arguments(parser):
     parser.add_argument("--tree", help="Newick tree", required=True)
     parser.add_argument("--alignment", nargs="+", help="sequence(s) to be used, supplied as FASTA files", required=True)
     parser.add_argument('--gene-names', nargs="+", type=str, help="names of the sequences in the alignment, same order assumed", required=True)
+    parser.add_argument("--compare-to", choices=["root", "ancestor", "pairwise"], help="type of comparison between samples in the given tree", required=True)
+    parser.add_argument("--attribute-name", help="name to store distances associated with the given distance map", required=True)
+    parser.add_argument("--map", help="JSON providing the distance map between sites and, optionally, amino acids at those sites", required=True)
+    parser.add_argument("--latest-date", help="latest date to consider samples for last ancestor or pairwise comparisons (e.g., 2019-01-01); defaults to the current date")
     parser.add_argument("--output", help="JSON file with calculated distances stored by node name and attribute name", required=True)
-    parser.add_argument("--attribute-names", nargs="+", help="names to store distances associated with the corresponding masks", required=True)
-    parser.add_argument("--masks", help="tab-delimited mask definitions with mask name in first column and binary mask in second column")
-    parser.add_argument("--mask-names", nargs="*", help="name of each mask to use from the given masks file. Distances are calculated for each given mask and stored in each of the corresponding attribute names. If no mask is provided, all sites will be used.")
 
 
 def run(args):
     # Load tree.
     tree = Bio.Phylo.read(args.tree, "newick")
-
-    # Identify the name of the root node.
-    root_node_name = tree.root.name
 
     # Load sequences.
     alignments = load_alignments(args.alignment, args.gene_names)
@@ -73,54 +210,66 @@ def run(args):
         for record in alignment:
             sequences_by_node_and_gene[record.name][gene] = str(record.seq)
 
-    # Create a single amino acid sequence for the root node based on the order of the genes.
-    # Convert the string into an array for mask comparisons.
-    root_node_sequence = np.fromstring(
-        "".join([
-            sequences_by_node_and_gene[root_node_name][gene]
-            for gene in args.gene_names
-        ]),
-        dtype="S1"
-    )
+    # Load the given distance map.
+    distance_map = read_distance_map(args.map)
 
-    # Determine which sites to include in distance calculations.
-    # If any masks are specified, calculate the distance for each mask and store it in the corresponding attribute name.
-    if args.masks:
-        # Load masks.
-        masks = read_masks(args.masks)
-
-        # Map masks to attribute names where distances will be stored.
-        attributes_by_mask = dict(zip(args.mask_names, args.attribute_names))
-    else:
-        # If no masks are specified, calculate the distance using all sites.
-        mask_name = "all_sites"
-        masks = {mask_name: np.ones(len(root_node_sequence)).astype(bool)}
-
-        # Use the requested attribute name to store distances for this mask.
-        attributes_by_mask = {mask_name: args.attribute_names[0]}
-
-    # Calculate Hamming distance between the root node's sequence and the single
-    # amino acid sequence for each node in the tree.
-    distances_by_node = {}
-    for node, node_translations in sequences_by_node_and_gene.items():
-        node_sequence = np.fromstring(
-            "".join([
-                node_translations[gene]
-                for gene in args.gene_names
-            ]),
-            dtype="S1"
+    # Use the distance map to calculate distances between all samples in the
+    # given tree and the desired target(s).
+    if args.compare_to == "root":
+        # Calculate distance between the root and all samples.
+        distances_by_node = get_distances_to_root(
+            tree,
+            sequences_by_node_and_gene,
+            distance_map
         )
+    elif args.compare_to == "ancestor":
+        # Calculate distance between the last ancestor for each sample in a
+        # previous season.
+        distances_by_node = get_distances_to_last_ancestor(
+            tree,
+            sequences_by_node_and_gene,
+            distance_map,
+            args.latest_date
+        )
+    elif args.compare_to == "pairwise":
+        # Calculate distance between each sample and all other samples in a
+        # previous season.
+        distances_by_node = get_pairwise_distances(
+            tree,
+            sequences_by_node_and_gene,
+            distance_map,
+            args.latest_date
+        )
+    else:
+        pass
 
-        # Calculate the distance for this node for each requested mask and store
-        # the result in the corresponding attribute name.
-        distances_by_node[node] = {}
-        for mask_name, attribute in attributes_by_mask.items():
-            distances_by_node[node][attribute] = mask_distance(
-                root_node_sequence,
-                node_sequence,
-                masks[mask_name]
-            )
+    # Map distances to the requested attribute name.
+    # Convert data like:
+    # {
+    #   "A/AbuDhabi/24/2017": 1
+    # }
+    # to data like:
+    #
+    # {
+    #   "A/AbuDhabi/24/2017": {
+    #     "ep": 1
+    #   }
+    # }
+    #
+    final_distances_by_node = {}
+    for node_name, values in distances_by_node.items():
+        final_distances_by_node[node_name] = {
+            args.attribute_name: values
+        }
+
+    # Prepare params for export.
+    params = {
+        "attribute": args.attribute_name,
+        "compare_to": args.compare_to,
+        "map_name": distance_map.get("name", args.map),
+        "latest_date": args.latest_date
+    }
 
     # Export distances to JSON.
     with open(args.output, "w") as oh:
-        json.dump({"nodes": distances_by_node}, oh, indent=1, sort_keys=True)
+        json.dump({"params": params, "nodes": final_distances_by_node}, oh, indent=1, sort_keys=True)
